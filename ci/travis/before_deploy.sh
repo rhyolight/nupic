6e5124963d9c35c905e807068b14777d735e660d
Matthew Taylor <matt@numenta.org>
2015-01-01 08:59:17 -0800

    printing versions

--- ci/travis/before_deploy.sh
+++ ci/travis/before_deploy.sh
@@ -26,33 +26,20 @@ echo
 
 cd ${TRAVIS_BUILD_DIR}
 
-# If this branch is master, this is an iterative deployment, so we'll package
-# wheels ourselves for deployment to S3. No need to build docs.
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
+echo "pip --version"
+pip --version
+echo "which pip"
+which pip
+echo "sudo pip --version"
+sudo pip --version
+echo "sudo which pip"
+sudo which pip
 
-    # Assuming pip 1.5.X is installed.
-    echo "sudo pip install wheel"
-    sudo pip install wheel
+# Assuming pip 1.5.X is installed.
+echo "sudo pip install wheel"
+sudo pip install wheel
 
-    # Wheel fails unless we remove this.
-    sudo rm -rf external/linux32arm
-
-    # Build all NuPIC and all required python packages into dist/wheels as .whl
-    # files.
-    echo "pip wheel --wheel-dir=dist/wheels ."
-    pip wheel --wheel-dir=dist/wheels .
-
-    # The dist/wheels folder is expected to be deployed to S3.
-
-# If this is a tag, we're doing a release deployment, so we want to build docs
-# for pypi...
-else
-
-    # For docs, direct people to numenta.org/docs/nupic.
-    mkdir ./build/docs
-    echo "<html><body>See NuPIC docs at <a href='http://numenta.org/docs/nupic/'>http://numenta.org/docs/nupic/</a>.</body></html>" > build/docs/index.html
-
-fi
-
-
+# Prove it works
+echo "pip help wheel"
+pip help wheel
 
